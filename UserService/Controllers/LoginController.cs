f8763451752fcce2c085f78a0f58e1f214fd4fcc
WiktoriaWezgraj <wiktoriawezgraj1@gmail.com>
2025-05-04 12:38:17 +0200

    Add JWT Authentication

---
+++ UserService/Controllers/LoginController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Authentication;
+using User.Application.Services;
+using User.Domain.Exceptions.Login;
+using User.Domain.Models.Requests;
+
+namespace UserService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        protected ILoginService _loginService;
+
+        public LoginController(ILoginService loginService)
+        {
+            _loginService = loginService;
+        }
+
+
+        [HttpPost]
+        public IActionResult Login([FromBody] User.Domain.Models.Requests.LoginRequest request)
+        {
+            try
+            {
+                var token = _loginService.Login(request.Username, request.Password);
+                return Ok(new { token });
+            }
+            catch (InvalidCredentialsException)
+            {
+                return Unauthorized();
+            }
+        }
+
+        [HttpGet]
+        [Authorize]
+        [Authorize(Policy = "AdminOnly")]
+        public IActionResult AdminPage()
+        {
+            return Ok();
+        }
+    }
+}
